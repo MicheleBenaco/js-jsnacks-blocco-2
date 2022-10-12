6c4dcfe2c129e37944c004e67735d7665d0c6b02
MicheleBenaco <benaco.michele@yahoo.it>
2022-10-12 17:09:35 +0200

    es2

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const bottone = document.getElementById('btn')
 const div = document.querySelector('.risultato')
 
  function calcolo (){
-    let datoUtente = document.getElementById('numeroUtente');
+    let datoUtente = document.getElementById('numeroUtente').value;
     console.log(datoUtente)
     if(datoUtente %2 == 0 ){
         div.innerHTML = "Numero Pari";
@@ -18,3 +18,19 @@ const div = document.querySelector('.risultato')
 
 }
 bottone.addEventListener('click',calcolo);
+// ---------------------- es2---------------
+// lista con nomi e di  cognomi
+// generare una falsa lista 
+
+
+let listaNome = ['Luca', 'Mario', 'Giuseppe', 'Martina'];
+let listaCognome = [ 'Rossi', 'Bianchi','Verdi','Napoli'];
+const listFalsa = document.querySelector('.listafalsa');
+
+let myRandomListaNomi = listaNome[Math.floor(Math.random() * listaNome.length)]
+let myRandomListaCognomi = listaCognome[Math.floor(Math.random() * listaCognome.length)]
+console.log(myRandomListaNomi,myRandomListaCognomi)
+let myElm = document.createElement("h2");
+myElm.innerText = myRandomListaNomi + " " + myRandomListaCognomi;
+document.body.appendChild(myElm);
+// ---------------------- es3---------------
